80a7bb6dc0b390b29d6f70b0c7c9aef562d70e3a
Shamil65 <shamilziganshin132@gmail.com>
2026-02-28 23:45:24 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher
+
+bot = Bot()             # Подкоючение к нашему боту
+dp = Dispatcher()       # Обработка updates/управляет входящими и исходящими сообщениями
+
+
